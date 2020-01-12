5d1eb48ff7cb8223b98ff39190378c4cff16b283
Simon <sstur@me.com>
2020-01-12 12:02:38 -0800

    Add ban-types rule

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/prefer-interface': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        types: {
+          Boolean: { message: 'Use boolean instead', fixWith: 'boolean' },
+          Number: { message: 'Use number instead', fixWith: 'number' },
+          String: { message: 'Use string instead', fixWith: 'string' },
+          Symbol: { message: 'Use symbol instead', fixWith: 'symbol' },
+        },
+      },
+    ],
 
     // ESLint (eslint-plugin-eslint-comments)
     'eslint-comments/no-unused-disable': 'warn',

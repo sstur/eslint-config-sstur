b51f0774effa7020f78ae00e904d577d7fc890ff
Simon <sstur@me.com>
2020-03-08 17:46:06 -0700

    Add eslint-plugin-import

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['eslint-comments', 'prettier'],
+  plugins: ['eslint-comments', 'import', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
   ],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
   rules: {
     // TypeScript (@typescript-eslint/eslint-plugin)
     '@typescript-eslint/array-type': ['error', { default: 'generic' }],
@@ -43,6 +46,15 @@ module.exports = {
 
     // ESLint (eslint-plugin-eslint-comments)
     'eslint-comments/no-unused-disable': 'warn',
+
+    // Import/export syntax (eslint-plugin-import)
+    'import/no-useless-path-segments': [
+      'warn',
+      {
+        noUselessIndex: true,
+      },
+    ],
+    'import/order': ['warn', { 'newlines-between': 'always' }],
 
     // Custom
     'array-callback-return': 'warn',
